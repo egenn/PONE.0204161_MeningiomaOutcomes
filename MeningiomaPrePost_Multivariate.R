18051b8f91dace558f7398eb476f9a5595d84d17
Stathis Gennatas <gennatas@gmail.com>
2018-11-06 17:13:38 -0800

    update for rtemis v0.74

--- MeningiomaPrePost_Multivariate.R
+++ MeningiomaPrePost_Multivariate.R
@@ -17,27 +17,27 @@ resampler <- rtSet.resampler("strat.sub", 100, seed = 4221)
 # GLM
 preop.lf.glm.100ss <- elevate(men$preop.lf, mod = "glm",
                               params = list(upsample = TRUE),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.lf_100ss/preop.lf_glm.100ss"))
 
 # GLMNET
 preop.lf.glmnet.100ss <- elevate(men$preop.lf, mod = "glmnet",
                                  params = list(upsample = TRUE),
-                                 resampler.rtSet = resampler, save.mods = FALSE,
+                                 resample.rtset = resampler, save.mods = FALSE,
                                  outdir = paste0(outpath, "/preop.lf_100ss/preop.lf_glmnet.100ss"))
 
 # SVM
 preop.lf.svm.100ss <- elevate(men$preop.lf, mod = "svm",
                               params = list(upsample = TRUE,
                                             gamma = c(1/17, 1/17*.1, 1/17*.01)),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.lf_100ss/preop.lf_svm.100ss"))
 
 # CART
 preop.lf.cart.100ss <- elevate(men$preop.lf, mod = "cart",
                                params = list(upsample = TRUE, maxdepth = 30,
                                              prune.cp = c(.1, .05, .01)),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/preop.lf_100ss/preop.lf_cart.100ss"))
 
 # MDB
@@ -46,13 +46,13 @@ preop.lf.mdb.100ss <- elevate(men$preop.lf, mod = "mdb",
                                             gamma = seq(.6, 1, .2),
                                             max.depth = 30,
                                             min.hessian = .001),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.lf_100ss/preop.lf_mdb.100ss"))
 
 # RF
 preop.lf.rf.100ss <- elevate(men$preop.lf, mod = "ranger",
                              params = list(upsample = TRUE),
-                             resampler.rtSet = resampler, save.mods = FALSE,
+                             resample.rtset = resampler, save.mods = FALSE,
                              outdir = paste0(outpath, "/preop.lf_100ss/preop.lf_rf.100ss"))
 
 # GBM
@@ -60,34 +60,34 @@ preop.lf.gbm.100ss <- elevate(men$preop.lf, mod = "gbm",
                               params = list(upsample = TRUE,
                                             interaction.depth = 1:2,
                                             n.minobsinnode = 2),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.lf_100ss/preop.lf_gbm.100ss"))
 
 # [ CONVENTIONAL LF ] ====
 # GLM
 postop.lf.glm.100ss <- elevate(men$postop.lf, mod = "glm",
                                params = list(upsample = TRUE),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.lf_100ss/postop.lf_glm.100ss"))
 
 # GLMNET
 postop.lf.glmnet.100ss <- elevate(men$postop.lf, mod = "glmnet",
                                   params = list(upsample = TRUE),
-                                  resampler.rtSet = resampler, save.mods = FALSE,
+                                  resample.rtset = resampler, save.mods = FALSE,
                                   outdir = paste0(outpath, "/postop.lf_100ss/postop.lf_glmnet.100ss"))
 
 # SVM
 postop.lf.svm.100ss <- elevate(men$postop.lf, mod = "svm",
                                params = list(upsample = TRUE,
                                              gamma = c(1/6, 1/6*.1, 1/6*.01)),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.lf_100ss/postop.lf_svm.100ss"))
 
 # CART
 postop.lf.cart.100ss <- elevate(men$postop.lf, mod = "cart",
                                 params = list(upsample = TRUE, maxdepth = 30,
                                               prune.cp = c(.1, .05, .01)),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/postop.lf_100ss/postop.lf_cart.100ss"))
 
 # MDB
@@ -96,13 +96,13 @@ postop.lf.mdb.100ss <- elevate(men$postop.lf, mod = "mdb",
                                              gamma = seq(.6, 1, .2),
                                              max.depth = 30,
                                              min.hessian = .001),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.lf_100ss/postop.lf_mdb.100ss"))
 
 # RF
 postop.lf.rf.100ss <- elevate(men$postop.lf, mod = "ranger",
                               params = list(upsample = TRUE),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/postop.lf_100ss/postop.lf_rf.100ss"))
 
 # GBM
@@ -110,34 +110,34 @@ postop.lf.gbm.100ss <- elevate(men$postop.lf, mod = "gbm",
                                params = list(upsample = TRUE,
                                              interaction.depth = 1:2,
                                              n.minobsinnode = 2),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.lf_100ss/postop.lf_gbm.100ss"))
 
 # [ INTEGRATED LF ] ====
 # GLM
 prepost.lf.glm.100ss <- elevate(men$prepost.lf, mod = "glm",
                                 params = list(upsample = TRUE),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.lf_100ss/prepost.lf_glm.100ss"))
 
 # GLMNET
 prepost.lf.glmnet.100ss <- elevate(men$prepost.lf, mod = "glmnet",
                                    params = list(upsample = TRUE),
-                                   resampler.rtSet = resampler, save.mods = FALSE,
+                                   resample.rtset = resampler, save.mods = FALSE,
                                    outdir = paste0(outpath, "/prepost.lf_100ss/prepost.lf_glmnet.100ss"))
 
 # SVM
 prepost.lf.svm.100ss <- elevate(men$prepost.lf, mod = "svm",
                                 params = list(upsample = TRUE,
                                               gamma = c(1/20, 1/20*.1, 1/20*.01)),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.lf_100ss/prepost.lf_svm.100ss"))
 
 # CART
 prepost.lf.cart.100ss <- elevate(men$prepost.lf, mod = "cart",
                                  params = list(upsample = TRUE, maxdepth = 30,
                                                prune.cp = c(.1, .05, .01)),
-                                 resampler.rtSet = resampler, save.mods = FALSE,
+                                 resample.rtset = resampler, save.mods = FALSE,
                                  outdir = paste0(outpath, "/prepost.lf_100ss/prepost.lf_cart.100ss"))
 
 # MDB
@@ -146,13 +146,13 @@ prepost.lf.mdb.100ss <- elevate(men$prepost.lf, mod = "mdb",
                                               gamma = seq(.6, 1, .2),
                                               max.depth = 30,
                                               min.hessian = .001),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.lf_100ss/prepost.lf_mdb.100ss"))
 
 # RF
 prepost.lf.rf.100ss <- elevate(men$prepost.lf, mod = "ranger",
                                params = list(upsample = TRUE),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/prepost.lf_100ss/prepost.lf_rf.100ss"))
 
 # GBM
@@ -160,34 +160,34 @@ prepost.lf.gbm.100ss <- elevate(men$prepost.lf, mod = "gbm",
                                 params = list(upsample = TRUE,
                                               interaction.depth = 1:2,
                                               n.minobsinnode = 2),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.lf_100ss/prepost.lf_gbm.100ss"))
 
 # [ PREOPERATIVE OS ] ====
 # GLM
 preop.os.glm.100ss <- elevate(men$preop.os, mod = "glm",
                               params = list(upsample = TRUE),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.os_100ss/preop.os_glm.100ss"))
 
 # GLMNET
 preop.os.glmnet.100ss <- elevate(men$preop.os, mod = "glmnet",
                                  params = list(upsample = TRUE),
-                                 resampler.rtSet = resampler, save.mods = FALSE,
+                                 resample.rtset = resampler, save.mods = FALSE,
                                  outdir = paste0(outpath, "/preop.os_100ss/preop.os_glmnet.100ss"))
 
 # SVM
 preop.os.svm.100ss <- elevate(men$preop.os, mod = "svm",
                               params = list(upsample = TRUE,
                                             gamma = c(1/17, 1/17*.1, 1/17*.01)),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.os_100ss/preop.os_svm.100ss"))
 
 # CART
 preop.os.cart.100ss <- elevate(men$preop.os, mod = "cart",
                                params = list(upsample = TRUE, maxdepth = 30,
                                              prune.cp = c(.1, .05, .01)),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/preop.os_100ss/preop.os_cart.100ss"))
 
 # MDB
@@ -196,13 +196,13 @@ preop.os.mdb.100ss <- elevate(men$preop.os, mod = "mdb",
                                             gamma = seq(.6, 1, .2),
                                             max.depth = 30,
                                             min.hessian = .001),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.os_100ss/preop.os_mdb.100ss"))
 
 # RF
 preop.os.rf.100ss <- elevate(men$preop.os, mod = "ranger",
                              params = list(upsample = TRUE),
-                             resampler.rtSet = resampler, save.mods = FALSE,
+                             resample.rtset = resampler, save.mods = FALSE,
                              outdir = paste0(outpath, "/preop.os_100ss/preop.os_rf.100ss"))
 
 # GBM
@@ -210,34 +210,34 @@ preop.os.gbm.100ss <- elevate(men$preop.os, mod = "gbm",
                               params = list(upsample = TRUE,
                                             interaction.depth = 1:2,
                                             n.minobsinnode = 2),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/preop.os_100ss/preop.os_gbm.100ss"))
 
 # [ CONVENTIONAL OS ] ====
 # GLM
 postop.os.glm.100ss <- elevate(men$postop.os, mod = "glm",
                                params = list(upsample = TRUE),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.os_100ss/postop.os_glm.100ss"))
 
 # GLMNET
 postop.os.glmnet.100ss <- elevate(men$postop.os, mod = "glmnet",
                                   params = list(upsample = TRUE),
-                                  resampler.rtSet = resampler, save.mods = FALSE,
+                                  resample.rtset = resampler, save.mods = FALSE,
                                   outdir = paste0(outpath, "/postop.os_100ss/postop.os_glmnet.100ss"))
 
 # SVM
 postop.os.svm.100ss <- elevate(men$postop.os, mod = "svm",
                                params = list(upsample = TRUE,
                                              gamma = c(1/6, 1/6*.1, 1/6*.01)),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.os_100ss/postop.os_svm.100ss"))
 
 # CART
 postop.os.cart.100ss <- elevate(men$postop.os, mod = "cart",
                                 params = list(upsample = TRUE, maxdepth = 30,
                                               prune.cp = c(.1, .05, .01)),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/postop.os_100ss/postop.os_cart.100ss"))
 
 # MDB
@@ -246,13 +246,13 @@ postop.os.mdb.100ss <- elevate(men$postop.os, mod = "mdb",
                                              gamma = seq(.6, 1, .2),
                                              max.depth = 30,
                                              min.hessian = .001),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.os_100ss/postop.os_mdb.100ss"))
 
 # RF
 postop.os.rf.100ss <- elevate(men$postop.os, mod = "ranger",
                               params = list(upsample = TRUE),
-                              resampler.rtSet = resampler, save.mods = FALSE,
+                              resample.rtset = resampler, save.mods = FALSE,
                               outdir = paste0(outpath, "/postop.os_100ss/postop.os_rf.100ss"))
 
 # GBM
@@ -260,34 +260,34 @@ postop.os.gbm.100ss <- elevate(men$postop.os, mod = "gbm",
                                params = list(upsample = TRUE,
                                              interaction.depth = 1:2,
                                              n.minobsinnode = 2),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/postop.os_100ss/postop.os_gbm.100ss"))
 
 # [ INTEGRATED OS ] ====
 # GLM
 prepost.os.glm.100ss <- elevate(men$prepost.os, mod = "glm",
                                 params = list(upsample = TRUE),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.os_100ss/prepost.os_glm.100ss"))
 
 # GLMNET
 prepost.os.glmnet.100ss <- elevate(men$prepost.os, mod = "glmnet",
                                    params = list(upsample = TRUE),
-                                   resampler.rtSet = resampler, save.mods = FALSE,
+                                   resample.rtset = resampler, save.mods = FALSE,
                                    outdir = paste0(outpath, "/prepost.os_100ss/prepost.os_glmnet.100ss"))
 
 # SVM
 prepost.os.svm.100ss <- elevate(men$prepost.os, mod = "svm",
                                 params = list(upsample = TRUE,
                                               gamma = c(1/20, 1/20*.1, 1/20*.01)),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.os_100ss/prepost.os_svm.100ss"))
 
 # CART
 prepost.os.cart.100ss <- elevate(men$prepost.os, mod = "cart",
                                  params = list(upsample = TRUE, maxdepth = 30,
                                                prune.cp = c(.1, .05, .01)),
-                                 resampler.rtSet = resampler, save.mods = FALSE,
+                                 resample.rtset = resampler, save.mods = FALSE,
                                  outdir = paste0(outpath, "/prepost.os_100ss/prepost.os_cart.100ss"))
 
 # MDB
@@ -296,13 +296,13 @@ prepost.os.mdb.100ss <- elevate(men$prepost.os, mod = "mdb",
                                               gamma = seq(.6, 1, .2),
                                               max.depth = 30,
                                               min.hessian = .001),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.os_100ss/prepost.os_mdb.100ss"))
 
 # RF
 prepost.os.rf.100ss <- elevate(men$prepost.os, mod = "ranger",
                                params = list(upsample = TRUE),
-                               resampler.rtSet = resampler, save.mods = FALSE,
+                               resample.rtset = resampler, save.mods = FALSE,
                                outdir = paste0(outpath, "/prepost.os_100ss/prepost.os_rf.100ss"))
 
 # GBM
@@ -310,5 +310,5 @@ prepost.os.gbm.100ss <- elevate(men$prepost.os, mod = "gbm",
                                 params = list(upsample = TRUE,
                                               interaction.depth = 1:2,
                                               n.minobsinnode = 2),
-                                resampler.rtSet = resampler, save.mods = FALSE,
+                                resample.rtset = resampler, save.mods = FALSE,
                                 outdir = paste0(outpath, "/prepost.os_100ss/prepost.os_gbm.100ss"))
